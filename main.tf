662d3eb49c0cb6726832e04d7d0dcca06f20d601
slloyd0621 <86664560+slloyd0621@users.noreply.github.com>
2021-06-30 15:07:00 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,41 @@
+#EXAMPLE code.
+
+terraform {
+  backend "remote" {
+    hostname = "iacdev.wpp.cloud"
+    organization = "WPP-Lab"
+
+    workspaces {
+      prefix = "app-slloyd0621-"
+    }
+  }
+}
+
+
+provider "google" {
+  project     = "tharris-vault-customer-pov"
+  region      = "europe-west2"
+  zone        = "europe-west2-a"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = "github-actions-${var.environment_tag}-${var.name}"
+  machine_type = "f1-micro"
+
+  tags = ["test", "git"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
